fdeeef26efb9a44234661eaf74ca563fb4b01a0d
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2025-02-05 14:45:05 -0300

    feat: add waves audio and change fish asset

--- fish.js
+++ fish.js
@@ -4,12 +4,14 @@ class Scene extends Phaser.Scene {
   preload() {
     this.load.image("background", "assets/bg_azul-claro.png");
     this.load.image("logo", "assets/logo-inteli_branco.png");
-    this.load.image("fish", "assets/peixes/concha.png");
+    this.load.image("fish", "assets/peixes/peixinho_azul.png");
+    this.load.image("buoy", "assets/boia.png");
   }
 
   create() {
     this.add.image(400, 300, "background");
     this.add.image(400, 525, "logo").setScale(0.5);
+    this.add.image(200, 200, "buoy").setScale(0.25);
 
     fish = this.add.image(400, 300, "fish").setOrigin(0.5, 0.5);
     fish.setFlip(true, false);
